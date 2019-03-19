8587d9159179cf8402b931d2c0e054ad742d2321
leticia godoy <let.godoy@live.com>
2019-03-19 17:44:59 -0300

    init

---
+++ src/params.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Dimensions } from 'react-native'
+
+const params = {
+    blockSize: 30,
+    borderSize: 5,
+    fontSize: 15,
+    headerRatio: 0.15,
+    difficultLevel: 0.1,
+    getColumnsAmount() {
+        const width = Dimensions.get('screen').width
+        return Math.floor(width/this.blockSize)
+    },
+    getRowsAmount() {
+        const totalHeight = Dimensions.get('screen').height
+        const boardHeight = totalHeight * (1 - this.headerRatio)
+        return Math.floor(boardHeight / this.blockSize)
+    }
+}
+
+export default params
